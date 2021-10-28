c8c8a97e4639f37afaf2a29d075dcb4d2cbc32b2
Tahsina Bintay Azam <52793150+tahsina-azam@users.noreply.github.com>
2021-10-28 01:44:52 -0700

    Shifted to MongoDB
    Can read data from the registration page and store it in the MongoDB database.

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const cors =require('cors');
+const bcrypt =require('bcryptjs');
+const jwt= require('jsonwebtoken')
 const { faRetweet } = require('@fortawesome/free-solid-svg-icons');
-const connection = require('./db1')
+const cookieParser = require('cookieparser')
+const connectDB = require('./db/db_user')
+const User = require('./model/model_user')
+const path = require('path');
+const dotenv = require('dotenv').config({ path: path.resolve(__dirname, './config.env') })
+
 
 const app = express();
 
-app.use(cors());
+connectDB();
+
+app.use(cors( {
+    credentials: true,
+    origin: ['http://localhost:3000']
+}));
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json()) // To parse the incoming requests with JSON payloads
@@ -14,17 +26,29 @@ app.get('/userinfo',(req,res)=>{
    res.send('list of all task')
 })
 
-app.post('/addUser',(req,res)=>{
-    const ADD_QUERY = `INSERT INTO infotable (name,email,department,registrationnumber,session,password) 
-    VALUES('${req.body.data.name}','${req.body.data.email}','${req.body.data.department}','${req.body.data.registration}','${req.body.data.session}','${req.body.data.password}')`
-    //console.log(req.body)
-    //res.send('you can add user')
-    connection.query(ADD_QUERY,(err)=>{
-        if(err) console.log(err)
-        else res.send('user has been added')
+app.post('/register', async (req, res) => {
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        registration: req.body.registration,
+        department:req.body.department,
+        session:req.body.session,
+        password: hashedPassword,
     })
+
+    const result = await user.save()
+
+    const {password, ...data} = await result.toJSON()
+
+    res.send(data)
 })
+
 
 app.listen(4000,()=>{
     console.log('running on port 4000');
 })
+
+module.exports = app;
