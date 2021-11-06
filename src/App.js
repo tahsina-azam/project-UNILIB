38e49ece7ddad50d29254eaac4b5f1399f5f8899
Tahsina Bintay Azam <52793150+tahsina-azam@users.noreply.github.com>
2021-11-06 01:06:37 -0700

    added new route for activation in App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from './components/pages/Home';
 import Services from './components/pages/Services';
 import LogIn from './components/pages/LogIn';
 import SignUp from './components/pages/SignUp';
-
+import Activation from './components/pages/Activation'
 
 function App() {
   return (
@@ -22,6 +22,8 @@ function App() {
            {LogIn} />
            <Route path='/sign-up' exact component=
            {SignUp} />
+           <Route path='/authentication/activation/:token' exact component=
+           {Activation} />
          </Switch>
     </Router>
     </>//will keep all the routers here.
