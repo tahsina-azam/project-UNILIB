cb4a7f5c9403d36c0ec4a648f44e698b8e3aa41f
Tahsina Bintay Azam <52793150+tahsina-azam@users.noreply.github.com>
2021-11-09 13:03:42 +0600

    footer modified that fixes #4

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Button } from 'react-bootstrap'
 import './Footer.css'
 
 function Footer() {
@@ -7,20 +6,9 @@ function Footer() {
         <div className='footer-container'>
             <section className="footer-subscription">
                 <p className="footer-subscription-heading">
-                    Join us to get update on the pdfs that are 
-                    recently added
-                </p>
-                <p className="footer-subscription-text">
-                    You can unsubscribe at any time.
-                </p>
-                <div className="input-areas">
-                    <form>
-                       <input type='email' name='email'
-                       placeholder="Your Email"
-                       className="footer-input"/> 
-                       <Button buttonStyle='btn--outline'>Subscribe</Button>
-                    </form>
-                </div>
+                    Join us to checkout some awesome books and notes that are 
+                    recently added and motivate yourself to study
+               </p>  
             </section>
         </div>
     )
