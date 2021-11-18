f09d5f6a9982c5a7205b3b085329c68818793f56
Owishiboo <nalamowishi999@gmail.com>
2021-11-18 15:45:18 +0600

    lint changes

--- src/components/pages/Forum.js
+++ src/components/pages/Forum.js
@@ -1,36 +1,41 @@
-import { useSubscription } from '@apollo/client';
-import gql from 'graphql-tag';
-import React from 'react';
-import { Card } from 'react-bootstrap';
+import { useSubscription } from "@apollo/client";
+import gql from "graphql-tag";
+import React from "react";
+import { Card } from "react-bootstrap";
 
 const Post = ({ message }) => {
-    return <Card  style={{ width: '18rem' }}>
-        <Card.Body>
-        <Card.Text>
-            {message}
-        </Card.Text>
-        </Card.Body>
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Body>
+        <Card.Text>{message}</Card.Text>
+      </Card.Body>
     </Card>
-}
+  );
+};
 
 const GET_POSTS_QUERY = gql`
-                 query MyQuery {
-                     posts {
-                         message
-                         id
-                     }
-                 }`;
+  query MyQuery {
+    posts {
+      message
+      id
+    }
+  }
+`;
 
 function Forum() {
-    const { data, loading, error } = useSubscription(GET_POSTS_QUERY)
+  const { data, loading, error } = useSubscription(GET_POSTS_QUERY);
 
-    if (loading) return <div>loading...</div>;
-    if (error) return <div>error!</div>
-    return (
-        <>
-            {data.posts.length === 0 ? <div>no posts, sorry</div> : data.posts.map(p => <Post key={p.id} message={p.message} />)}
-        </>
-    )
+  if (loading) return <div>loading...</div>;
+  if (error) return <div>error!</div>;
+  return (
+    <>
+      {data.posts.length === 0 ? (
+        <div>no posts, sorry</div>
+      ) : (
+        data.posts.map((p) => <Post key={p.id} message={p.message} />)
+      )}
+    </>
+  );
 }
 
 export default Forum;
--- src/config/graphql.js
+++ src/config/graphql.js
@@ -1,11 +1,8 @@
-import {
-  ApolloClient,
-  InMemoryCache,
-} from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'https://hasura-reddit.herokuapp.com/v1/graphql',
-  cache: new InMemoryCache()
+  uri: "https://hasura-reddit.herokuapp.com/v1/graphql",
+  cache: new InMemoryCache(),
 });
 
 export default client;
