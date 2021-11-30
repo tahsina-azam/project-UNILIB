34e751bcfa514498f167cc1795f024db351c6f11
Owishiboo <nalamowishi999@gmail.com>
2021-11-30 19:12:59 +0600

    fixes #16 #17 and half of #15

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
 import "./styles/App.css";
 import Home from "./components/pages/Home";
 import Services from "./components/pages/Services";
@@ -9,29 +14,30 @@ import SignUp from "./components/pages/SignUp";
 import Activation from "./components/pages/Activation";
 import Forum from "./components/pages/Forum";
 import { ApolloProvider } from "@apollo/client";
-import "bootstrap/dist/css/bootstrap.min.css";
 import client from "./config/graphql";
+import StudentBooks from "./components/pages/StudentBooks";
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Router>
+      <BrowserRouter>
         <Navbar />
         <div className="pt-5">
-          <Switch>
-            <Route path="/forum" exact component={Forum} />
-            <Route path="/" exact component={Home} />
-            <Route path="/services" exact component={Services} />
-            <Route path="/log-in" exact component={LogIn} />
-            <Route path="/sign-up" exact component={SignUp} />
+          <Routes>
+            <Route path="/forum" element={<Forum />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/log-in" element={<LogIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/forum/getBooks" element={<StudentBooks />} />
             <Route
               path="/authentication/activation/:token"
               exact
               component={Activation}
             />
-          </Switch>
+          </Routes>
         </div>
-      </Router>
+      </BrowserRouter>
     </ApolloProvider> //will keep all the routers here.
   );
 }
