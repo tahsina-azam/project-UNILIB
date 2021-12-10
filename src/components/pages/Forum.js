c417c2fc21c9719e77c1d30a5800906ea98df0f3
Tahsina Bintay Azam <52793150+tahsina-azam@users.noreply.github.com>
2021-12-10 11:42:32 +0600

    retrived data of the user from the cookies in Forum

--- src/components/pages/Forum.js
+++ src/components/pages/Forum.js
@@ -1,10 +1,33 @@
+import React, { useState, useEffect } from "react";
 import GetPosts from "../GetPosts";
 import { TypePost } from "../PutPosts";
 import Sidebar from "../Sidebar";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import axios from "../../utility";
 
 function Forum() {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [department, setDepartment] = useState("");
+  const [session, setSession] = useState("");
+  const [registration, setRegistration] = useState("");
+
+  axios.get("http://localhost:4000/user", { withCredentials: true }).then(
+    (response) => {
+      console.log(response.data);
+      setEmail(response.data.data.email);
+      setName(response.data.data.name);
+      setDepartment(response.data.data.department);
+      setRegistration(response.data.data.registration);
+      setSession(response.data.data.session);
+    },
+    (error) => {
+      alert("You are not logged in.");
+    }
+  );
+
+  console.log(email); //you can directly address variables now by their names :D
+
   return (
     <div>
       <Sidebar />
@@ -19,4 +42,3 @@ function Forum() {
 }
 
 export default Forum;
-// className="fw-light fnt text-muted" no comments div
