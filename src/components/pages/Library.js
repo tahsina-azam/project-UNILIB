45ef509f020e6b207faf3673149a398025d5d4b3
Tahsina Bintay Azam <52793150+tahsina-azam@users.noreply.github.com>
2021-12-20 01:54:18 +0600

    library for students

--- src/components/pages/Library.js
+++ src/components/pages/Library.js
@@ -1,11 +1,44 @@
 import React from "react";
+import { Row, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ShowBookList from "./ShowBookList";
+import "../../styles/AdminLibrary.css";
 
 const App = () => {
   return (
     <div>
-      <h1 style={{ fontFamily: "Bahnschrift Light", color: "black" }}>
-        hello this is lilbrary
-      </h1>
+      <div class="">
+        <Row className="mb-5"></Row>
+        <div className=" container mx-auto">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="input-group mb-3">
+                {" "}
+                <input
+                  type="text"
+                  class="form-control input-text"
+                  placeholder="Search books...."
+                  aria-label="Recipient's username"
+                  aria-describedby="basic-addon2"
+                />
+                <div class="input-group-append">
+                  {" "}
+                  <button
+                    color="black"
+                    class="btn btn-outline-warning btn-lg"
+                    type="button"
+                  >
+                    <i style={{ color: "black" }} class="fa fa-search"></i>
+                  </button>{" "}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="mx-auto">
+        <StudentBookList />
+      </div>
     </div>
   );
 };
